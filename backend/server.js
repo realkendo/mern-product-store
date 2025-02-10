ca864aaf500d7617873435ee851e45e01346bd32
realkendo <kennethistifanus1@gmail.com>
2025-02-10 23:05:07 +0100

    Nodemon installation

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,5 @@ import express from "express";
 const app = express();
 
 app.listen(5000, () => {
-  console.log("Server started at https://localhost:5000");
+  console.log("Server started @ https://localhost:5000");
 });
