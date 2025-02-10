78341b6225b22edaeadff83ca52ea0f901725238
realkendo <kennethistifanus1@gmail.com>
2025-02-11 00:10:22 +0100

    model

---
+++ backend/models/product.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    Image: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // for createdAt and updatedAt
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
