75bf922922988120177751c815da09d7e4ede33e
realkendo <kennethistifanus1@gmail.com>
2025-02-11 17:02:56 +0100

    "crud routing format competed"

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,18 @@ const app = express();
 // middleware for parsing json data
 app.use(express.json());
 
-// root route
-// app.get("/", (req, res) => {
-//   res.send("Server is running...");
-// });
+// get route for all products
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log(`Error in fetching products: ${error.message}`);
+    res.status(500).json({ sucess: false, message: "Server Error" });
+  }
+});
 
+// post route for product
 app.post("/api/products", async (req, res) => {
   const product = req.body; //user will send this data
 
@@ -36,6 +43,44 @@ app.post("/api/products", async (req, res) => {
     res
       .status(500)
       .json({ success: false, message: "Server Error", error: error.message });
+  }
+});
+
+// Update route for a product
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const product = req.body;
+
+  // catching the 404 error
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ sucess: false, messsage: "Product ID Not Found" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
+
+// delete route for product
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res
+      .status(200)
+      .json({ success: true, message: "Product deleted successfully" });
+  } catch (error) {
+    console.log(`Error in deleting product: ${error.message}`);
+    res.status(404).json({ success: false, message: "Product Not Found" });
   }
 });
 
